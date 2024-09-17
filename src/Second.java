c423c75d6173d0859b58fbb99cdc0245f3e8ee81
00Unreal00 <ermakovnk@gmail.com>
2024-09-17 22:23:46 +0300

    Second okay + Task1/Task2

--- src/Second.java
+++ src/Second.java
@@ -1,11 +1,10 @@
 public class Second {
     public static void main(String[] args){
-        double summa = 0;
+        double summa = 1;
         double element = 1;
         for (int i = 0 ; i <= 8; i++){
-            summa += element;
             element = element/3;
-
+            summa += element;
         }
     System.out.println("Сумма:" + summa);
 
